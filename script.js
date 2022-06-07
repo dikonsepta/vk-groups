b460a33c146664496fcaf667a6ac1463b0735cf4
Diana Konsepta <di.konsepta@gmail.com>
2022-06-07 16:07:11 +0400

    Корректировка сортировки

--- script.js
+++ script.js
@@ -1,64 +1,61 @@
 VK.init({
-    apiId: 8146587
-});
-
+    apiId: 8146587,
+})
 
 function auth() {
     return new Promise((resolve, reject) => {
         VK.Auth.login(data => {
             if (data.session) {
-                resolve();
+                resolve()
             } else {
-                reject(new Error('Не удалось авторизоваться'));
+                reject(new Error('Не удалось авторизоваться'))
             }
-        }, 262144); // 262144 — доступ к группам пользователя
-    });
-}
-
-
-function callAPI(method, params) {
-    params.v = '5.81'; // версия
-
-    return new Promise((resolve, reject) => {
-        VK.api(method, params, (data) => {
-            if (data.error) {
-                reject(data.error);
-            } else {
-                resolve(data.response);
-            }
-        });
+        }, 262144) // 262144 — доступ к группам пользователя
     })
 }
 
+function callAPI(method, params) {
+    params.v = '5.81' // версия
 
-(async () => {
+    return new Promise((resolve, reject) => {
+        VK.api(method, params, data => {
+            if (data.error) {
+                reject(data.error)
+            } else {
+                resolve(data.response)
+            }
+        })
+    })
+}
+
+;(async () => {
     try {
-        await auth();
+        await auth()
 
-        document.body.onclick = (e) => {
-            const elem = e.target;
-            if (elem.getAttribute("data-role") === "clipboard") {
-                const text = elem.previousElementSibling.innerHTML;
-                console.log(text);
-                navigator.clipboard.writeText(text);
+        document.body.onclick = e => {
+            const elem = e.target
+            if (elem.getAttribute('data-role') === 'clipboard') {
+                const text = elem.previousElementSibling.innerHTML
+                console.log(text)
+                navigator.clipboard.writeText(text)
             }
         }
 
         const groups = await callAPI('groups.get', {
             // fields: 'id, name, screen_name, photo_200',
-            extended: 1
-        });
+            extended: 1,
+        })
 
         groups.items.sort(function (a, b) {
-            if (a.name < b.name) return -1
-        });
+            if (a.name.toLowerCase() < b.name.toLowerCase()) return -1
+        })
 
-        const template = document.querySelector('#groups-template').textContent;
-        const render = Handlebars.compile(template);
-        const html = render(groups);
-        const results = document.querySelector('#results');
-        results.innerHTML = html;
+        const template = document.querySelector('#groups-template').textContent
+        const render = Handlebars.compile(template)
+        const html = render(groups)
+        const results = document.querySelector('#results')
+        results.innerHTML = html
     } catch (e) {
-        console.log(e);
+        console.log(e)
     }
-})();
+})()
